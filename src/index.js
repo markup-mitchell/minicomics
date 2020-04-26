e2d1fa0bb37a7a1bd540a7d1bbc1c18d1ad7f79c
spitchell <mr.spitchell@gmail.com>
2020-04-26 12:54:14 +0100

    slowly slowly eliminating pipeline issues

--- src/index.js
+++ src/index.js
@@ -6,11 +6,30 @@ if (!fs.existsSync(config.dev.outDir)) fs.mkdirSync(config.dev.outDir);
 
 const editions = fs.readdirSync(config.dev.editionsDir);
 
-const createPage = (name) => {
+const createPageData = (name) => {
   let pageData = {};
   pageData.zineName = name;
   pageData.frames = fs.readdirSync(`${config.dev.editionsDir}/${name}`);
   return pageData;
 };
 
-console.log(pageHtml(createPage('home learning')));
+const createAllPages = (editions) => {
+  editions.forEach((edition) => {
+    let editionPath = `${config.dev.outDir}/${edition}`;
+    if (!fs.existsSync(editionPath)) {
+      fs.mkdirSync(editionPath);
+    }
+    fs.writeFile(
+      `${editionPath}/index.html`,
+      pageHtml(createPageData(edition)),
+      (e) => {
+        if (e) throw e;
+        console.log(`${editionPath}/index.html was created successfully`);
+      },
+    );
+    console.log(editionPath);
+  });
+};
+
+logstuff = (string) => string;
+createAllPages(editions);
--- src/page-html-function.js
+++ src/page-html-function.js
@@ -1,24 +1,25 @@
 const pageHtml = (data) => {
-  let images = data.frames
-    .map((frame) => `<img src="./content/${frame}" loading="lazy" alt="">`)
-    .join('\n');
+  console.log(data);
+  //   let images = data.frames
+  //     .map((frame) => `<img src="./content/${frame}" loading="lazy" alt="">`)
+  //     .join('\n');
 
-  return `
-   <!DOCTYPE html>
-   <html lang="en">
-     <head>
-       <meta charset="UTF-8" />
-       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-       <link rel="stylesheet" href="./style.css" />
-       <title>${data.zineName}</title>
-     </head>
-     <body>
-      <main>
-         ${images}
-      </main>
-     </body>
-   </html>
-   `;
+  //   return `
+  //    <!DOCTYPE html>
+  //    <html lang="en">
+  //      <head>
+  //        <meta charset="UTF-8" />
+  //        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  //        <link rel="stylesheet" href="./style.css" />
+  //        <title>${data.zineName}</title>
+  //      </head>
+  //      <body>
+  //       <main>
+  //          ${images}
+  //       </main>
+  //      </body>
+  //    </html>
+  //    `;
 };
 
 module.exports = pageHtml;
